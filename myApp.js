9b6c43a8e3fe363a86e60deb6fbdbc80a23e49d7
MarvinHezron <marvinrozen@gmail.com>
2021-08-12 22:36:28 +0300

    findById

--- myApp.js
+++ myApp.js
@@ -50,7 +50,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
